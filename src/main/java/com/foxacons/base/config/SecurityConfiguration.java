f460d071d02e4d292d5ac8055351ebbc27c55b92
AmanDeepak08 <58861982+CSPARAMAN@users.noreply.github.com>
2022-08-07 23:30:15 +0530

    Update SecurityConfiguration.java

--- src/main/java/com/foxacons/base/config/SecurityConfiguration.java
+++ src/main/java/com/foxacons/base/config/SecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/getAllProjects").antMatchers("/getAllContactDetails");
+		web.ignoring().antMatchers("/getAllProjects").antMatchers("/getAllContactDetails").antMatchers("/addContact");
 	}
 	
 	
